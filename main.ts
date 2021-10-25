01065e96c1d6191d2679700789082eb8710fcedc
Michael-Afton <93159594+Michael-Afton@users.noreply.github.com>
2021-10-25 15:32:03 -0500

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    game.splash("I AM VICTORIOUS")
+    game.splash("Why do you kill us?")
+    mySprite = sprites.create(assets.image`death`, SpriteKind.Player)
     game.reset()
 })
+let mySprite: Sprite = null
 scene.setBackgroundColor(10)
 let crewmate = sprites.create(assets.image`CREWMATE`, SpriteKind.Player)
 let imposter = sprites.create(assets.image`IMPOSTOR`, SpriteKind.Enemy)
